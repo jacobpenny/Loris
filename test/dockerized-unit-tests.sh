fad625af7cde8fadd9f3aaa93cc03f47a7cc9349
Jacob Penny <jacob.penny@gmail.com>
2016-11-04 12:17:13 -0400

    WIP

---
+++ test/dockerized-unit-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# TODO test watch feature
+
+# Try to docker-compose start, if it fails, do a docker-compose up
+if docker-compose start db > /dev/null 2>&1 ; then
+  echo -e 'Using an existing database container. If you would like a fresh instance run `docker-compose down` before running these tests.\n'
+else
+  echo 'Spinning up a fresh database... This may take a minute.'
+  docker-compose up -d db > /dev/null 2>&1
+fi
+
+while ! mysqladmin ping -h 127.0.0.1 -u SQLTestUser --password="TestPassword" --silent > /dev/null 2>&1; do
+    sleep 1
+done
+echo 'Database ready, running unit tests...'
+
+export LORIS_DB_CONFIG=config.xml
+../vendor/bin/phpunit --configuration phpunit.xml --testsuite 'LorisUnitTests for php/libraries'
+
+function finish {
+  echo 'Spinning down the database...'
+  docker-compose stop > /dev/null 2>&1
+  echo 'Done'
+}
+trap finish EXIT
