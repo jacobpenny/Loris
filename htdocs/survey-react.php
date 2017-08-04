bea82353856c422fa309994baea4fbcce861d5b5
zainvirani <zainilabideen.virani@mail.mcgill.ca>
2017-08-04 11:52:41 -0400

    context

--- htdocs/survey-react.php
+++ htdocs/survey-react.php
@@ -239,16 +239,19 @@ class DirectDataEntryMainPage
      */
     function display()
     {
+        $DB = \Database::singleton();
         $this->logRequest();
         $factory = \NDB_Factory::singleton();
         $config  = $factory->config();
         $json_class = new NDB_BVL_Instrument_JSON();
+        $json_class->setup($this->CommentID);
         $base    = $config->getSetting('base');
         $json    = file_get_contents($base."project/instruments/$this->TestName.json");
         $this->updateStatus('In Progress');
         $this->tpl_data['lang'] = $json_class->_getLang();
         $this->tpl_data['json'] = htmlspecialchars($json);
-        $this->tpl_data['initialData'] = $_REQUEST['initialData'];
+        //$this->tpl_data['initialData'] = $json_class->_getInstrumentData($DB);
+        echo serialize($json_class->_getContext());
         $contextArray = $json_class->_getContext();
         $contextJSON = json_encode($contextArray);
         $this->tpl_data['context'] = htmlspecialchars($contextJSON);
