4ea2ab0af2f9ad8ef7e245c7b2bc48cf8024cffd
zainvirani <zainilabideen.virani@mail.mcgill.ca>
2017-08-31 14:20:49 -0400

    new banner + logo + date + margins

--- htdocs/js/modules/direct-entry-react.js
+++ htdocs/js/modules/direct-entry-react.js
@@ -10,17 +10,24 @@ window.onload = function() {
   const context = JSON.parse(instrumentEl.dataset.context);
   const initialData = JSON.parse(instrumentEl.dataset.initial);
   const lang = instrumentEl.dataset.lang;
+  const logo = instrumentEl.dataset.logo ? instrumentEl.dataset.logo : "";
   const options = { surveyMode: true };
-
+  const curDate = new Date();
+  const curDateSt = (curDate.getMonth()+1) + "/" + curDate.getDate() + "/" + curDate.getFullYear();
   ReactDOM.render(
-    <InstrumentFormContainer
-      instrument={instrument}
-      initialData={initialData}
-      lang={lang}
-      context={context}
-      options={options}
-      onSave={onSave}
-    />,
+    <div>
+      <img id="banner" className="banner"/>
+      <img id="logo" src={logo}/>
+      <div><font color="white">Date: {curDateSt}</font></div>
+      <InstrumentFormContainer
+        instrument={instrument}
+        initialData={initialData}
+        lang={lang}
+        context={context}
+        options={options}
+        onSave={onSave}
+      />
+    </div>,
     document.getElementById("container")
   );
 };
