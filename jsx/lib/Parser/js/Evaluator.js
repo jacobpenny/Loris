87c33eaa9e8e9286dd7637c5d264ead0428a2620
zainvirani <zainilabideen.virani@mail.mcgill.ca>
2018-06-13 18:13:34 -0400

    support checkboxes on JSON instruments

--- jsx/lib/Parser/js/Evaluator.js
+++ jsx/lib/Parser/js/Evaluator.js
@@ -28,10 +28,10 @@ function evalAST(tree, scope) {
       var res = scope[tree.args[0]];
       for (var i = 0; i < tree.args[1].length; i++) {
         if (typeof res[tree.args[1][i]] === 'undefined') {
-          throw new UndefinedVariableError(`Unbound sub-variable: ${tree.args[0]}`);
+          throw new UndefinedVariableError(`Unbound sub-variable: ${tree.args[1]}`);
         }
         if (res[tree.args[1][i]] === null) {
-          throw new NullVariableError(`Null sub-variable: ${tree.args[0]}`);
+          throw new NullVariableError(`Null sub-variable: ${tree.args[1]}`);
         }
         res = res[tree.args[1][i]];
       }
